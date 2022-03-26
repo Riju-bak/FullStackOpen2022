86bc719f61b964b2fc3f870704e0b7058bafafbd
Riju-bak <defaltsith@gmail.com>
2022-03-26 15:03:59 -0400

    upto 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,30 +1,33 @@
-import Header from './Header';
-import Content from './Content';
-import Total from "./Total";
+import {useState} from "react";
+import Button from "./Button";
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const handleGood = () => {
+        setGood(good+1);
+    };
+
+    const handleNeutral = () => {
+        setNeutral(neutral+1);
+    };
+
+    const handleBad = () => {
+        setBad(bad+1);
     }
+
     return (
         <>
-            <Header course={course}/>
-            <Content course = {course}/>
-            <Total course={course}/>
+            <h1>give feedback</h1>
+            <Button onClick={handleGood} text={"good"}/>
+            <Button onClick={handleNeutral} text={"neutral"}/>
+            <Button onClick={handleBad} text={"bad"}/>
+            <h1>Statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
         </>
     );
 };
