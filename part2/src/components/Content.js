6ecc24dbc8c32f861e6fbb57fe3c7c2b5864bbd2
Riju-bak <defaltsith@gmail.com>
2022-04-08 16:21:29 -0400

    upto 2.3

--- part2/src/components/Content.js
+++ part2/src/components/Content.js
@@ -1,8 +1,16 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({parts}) =>
     <>
-        {parts.map(part => <Part part={part}/>)}
+        {parts.map(part => <Part key={part.id} part={part}/>)}
+        <Total
+            sum={
+                parts.reduce(
+                    (sum, part) => sum + part.exercises,
+                    0)
+            }
+        />
     </>
 
 export default Content;
